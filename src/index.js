219ab79d502105dc0c9da3b1d522d0de2565b94c
tienvhan <63879235+tienvhan@users.noreply.github.com>
2020-04-17 13:57:29 -0700

    Unsplash key working
    Able to pull the resulted images from Unsplash using access key

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
+import axios from "axios";
 
 function App() {
+  const [photo, setPhoto] = useState("");
+  const [clientID, setClientID] = useState(
+    "04RXO8uHdsa2-qLy3ydVG6NuzDGq-T_rsmM4OcXFCME"
+  );
+
+  function handleChange(event) {
+    setPhoto(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    console.log(photo);
+
+    const url =
+      "https://api.unsplash.com/search/photos?page=1&query=" +
+      photo +
+      "&client_id=" +
+      clientID;
+
+    axios.get(url).then(response => {
+      console.log(response);
+    });
+  }
+
   return (
     <div className="App">
       <h1>Unsplash Photo Search</h1>
@@ -10,8 +34,15 @@ function App() {
         Apfter the results are displayed click any image to use it as the
         website's background
       </h2>
-      <input name="photo" type="text" placeholder="Search for photos" />
-      <button type="submit">Search</button>
+      <input
+        onChange={handleChange}
+        name="photo"
+        type="text"
+        placeholder="Search for photos"
+      />
+      <button onClick={handleSubmit} type="submit">
+        Search
+      </button>
     </div>
   );
 }
